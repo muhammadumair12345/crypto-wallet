905189d75f758128441dc6b6186e6b35cb1aa7a2
Muhammad Umair <hafizumairtariq1234@gmail.com>
2023-07-20 18:44:41 +0500

    updated

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,45 +1,40 @@
-const TransactionsCard = () => {
+import dummyData from "../utils/dummyData";
+import { shortenAddress } from "../utils/shortenAddress";
+
+const TransactionsCard = (prop) => {
+  const { addressTo, addressFrom, timestamp, message, amount, url } = prop;
+
   return (
-    <div
-      className="bg-[#181918] m-4 flex flex-1
-      2xl:min-w-[450px]
-      2xl:max-w-[500px]
-      sm:min-w-[270px]
-      sm:max-w-[300px]
-      min-w-full
-      flex-col p-3 rounded-md hover:shadow-2xl"
-    >
-      <div className="flex flex-col items-center w-full mt-3">
-        <div className="display-flex justify-start w-full mb-6 p-2">
-          <a
-            href={`https://ropsten.etherscan.io/address/${""}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <p className="text-white text-base">From: {"..."}</p>
-          </a>
-          <a
-            href={`https://ropsten.etherscan.io/address/${""}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <p className="text-white text-base">To: {"..."}</p>
-          </a>
-          <p className="text-white text-base">Amount: {"..."} ETH</p>
+    <div className="flex flex-col gap-6 p-6 shadow-md rounded-2xl border-[0.5px] border-secondary">
+      <div>
+        <a
+          href={`https://ropsten.etherscan.io/address/${addressFrom}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p className="text-base">From: {shortenAddress(addressFrom)}</p>
+        </a>
+        <a
+          href={`https://ropsten.etherscan.io/address/${addressTo}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p className="text-base">To: {shortenAddress(addressTo)}</p>
+        </a>
+        <p className="text-base">Amount: {amount} ETH</p>
+        {message && (
           <>
             <br />
-            <p className="text-white text-base">Message: {message}</p>
+            <p className="text-base">Message: {message}</p>
           </>
-        </div>
-        <img
-          src={""}
-          alt="nature"
-          className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
-        />
-        <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-          <p className="text-[#37c7da] font-bold">{""}</p>
-        </div>
+        )}
       </div>
+      <img
+        src={url}
+        alt="nature"
+        className="w-full h-64 rounded-md object-cover"
+      />
+      <p className="text-primary font-bold">{timestamp}</p>
     </div>
   );
 };
@@ -47,23 +42,19 @@ const TransactionsCard = () => {
 const Transactions = () => {
   const currentAccount = false;
   return (
-    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
-      <div className="flex flex-col md:p-12 py-12 px-4">
-        {currentAccount ? (
-          <h3 className="text-white text-3xl text-center my-2">
-            Latest Transactions
-          </h3>
-        ) : (
-          <h3 className="text-white text-3xl text-center my-2">
-            Connect your account to see the latest transactions
-          </h3>
-        )}
+    <div className="py-12 flex flex-col gap-6">
+      {currentAccount ? (
+        <h3 className="text-3xl text-center">Latest Transactions</h3>
+      ) : (
+        <h3 className="text-3xl text-center">
+          Connect your account to see the latest transactions
+        </h3>
+      )}
 
-        <div className="flex flex-wrap justify-center items-center mt-10">
-          {[].reverse().map((transaction, i) => (
-            <TransactionsCard key={i} {...transaction} />
-          ))}
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-between">
+        {dummyData.reverse().map((transaction, i) => (
+          <TransactionsCard key={i} {...transaction} />
+        ))}
       </div>
     </div>
   );
